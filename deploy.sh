a7832c6a56f65955f98292e31179412625b5c81b
Duane McGuire <biz@duanemcguire.com>
2026-02-28 19:59:14 -0700

    dcc

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ cd ~/code/wa7pge-flask
 source venv/bin/activate
 python -m pip install -r requirements.txt
 python sitebuilder.py build
-#cp _redirects build/_redirects
+cp _redirects build/_redirects
 npx -y pagefind --site build
 netlify deploy --prod --dir build 
